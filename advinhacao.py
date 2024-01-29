b38f970a6ab64742bf0c48570d454e21d3d2b8f8
Robeto Melo <rmello.filho@hotmail.com>
2024-01-29 19:05:01 -0300

    aprimorando o print

--- advinhacao.py
+++ advinhacao.py
@@ -8,6 +8,6 @@ numero_secreto = 42
 chute = int(input("Digite o seu número: "))
 
 if (chute == numero_secreto):
-    print("Você acertou!")
+    print("Você acertou!", "\nO número secreto é,", chute)
 else:
-    print("Você errou!")
+    print("Você errou!", "\nO número que você digitou foi o", chute)
