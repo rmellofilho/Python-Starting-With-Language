036cfaa31afc5375daaaa04e245b9785c328d68b
Robeto Melo <rmello.filho@hotmail.com>
2024-01-29 19:09:40 -0300

    melhorando dicas e retorno do if

--- advinhacao.py
+++ advinhacao.py
@@ -8,6 +8,10 @@ numero_secreto = 42
 chute = int(input("Digite o seu número: "))
 
 if (chute == numero_secreto):
-    print("Você acertou!", "\nO número secreto é,", chute)
+    print("Você acertou!", "\nO número secreto é o", chute, end="!")
+elif(chute > numero_secreto):
+    print("Você errou!", "\nO número que você digitou foi o", chute, ", ele é maior que o número secreto!")
+elif(chute < numero_secreto):
+    print("Você errou!", "\nO número que você digitou foi o", chute, ", ele é menor que o número secreto!")
 else:
     print("Você errou!", "\nO número que você digitou foi o", chute)
