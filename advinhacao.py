c247b6c8c49ad39dd11ad98b743db015fa13b665
Robeto Melo <rmello.filho@hotmail.com>
2024-02-01 19:56:31 -0300

    refatorando e colocando pontuação

--- advinhacao.py
+++ advinhacao.py
@@ -8,6 +8,7 @@ print("===============================")
 numero_secreto = round(random.randrange(1, 101))
 total_de_tentativas = 0
 nivel = int(input("Defina o nível: "))
+pontos = 100
 
 
 if(nivel == 1):
@@ -17,9 +18,13 @@ elif(nivel == 2):
 else:
     total_de_tentativas = 5
 
+
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+
     chute = int(input("Digite um número entre 1 e 100: "))
+
+
 
     if(chute < 1 or chute > 100):
         print("Você deve digitar um número entre 1 e 100!")
@@ -32,12 +37,14 @@ for rodada in range(1, total_de_tentativas + 1):
     if (acertou):
         print("Você acertou!", "\nO número secreto é o", chute, end="!")
         break
-    elif(maior):
-        chute        
-        print("Você errou!", "\nO seu chute foi", chute, "e ele é maior que o número secreto!", "\nTente novamente.")
-    elif(menor):
-        chute
-        print("Você errou!", "\nO seu chute foi", chute, "e ele é menor que o número secreto!", "\nTente novamente.")
+    else:
+        if(maior):  
+            print("Você errou!", "\nO seu chute foi", chute, "e ele é maior que o número secreto!", "\nTente novamente.")
+        elif(menor):
+            print("Você errou!", "\nO seu chute foi", chute, "e ele é menor que o número secreto!", "\nTente novamente.")
+
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
 print("\n")
 
 print("Fim de jogo!")
