58fd46d622e232b9d163e73e7d53fa31565ad09d
Robeto Melo <rmello.filho@hotmail.com>
2024-02-01 17:37:38 -0300

    validação de entrada

--- advinhacao.py
+++ advinhacao.py
@@ -9,7 +9,11 @@ total_de_tentativas = 3
 
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    chute = int(input("Digite o seu número: "))
+    chute = int(input("Digite um número entre 1 e 100: "))
+
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
 
     acertou = chute == numero_secreto
     maior   = chute > numero_secreto
