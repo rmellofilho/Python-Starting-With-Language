341f36d77aad88a72f25fa1ac482e5dfe2c36ddf
Robeto Melo <rmello.filho@hotmail.com>
2024-02-01 17:28:48 -0300

    break no acertou

--- advinhacao.py
+++ advinhacao.py
@@ -17,6 +17,7 @@ for rodada in range(1, total_de_tentativas + 1):
 
     if (acertou):
         print("Você acertou!", "\nO número secreto é o", chute, end="!")
+        break
     elif(maior):
         chute        
         print("Você errou!", "\nO seu chute foi", chute, "e ele é maior que o número secreto!", "\nTente novamente.")
