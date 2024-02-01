113493291a8f0c036a6d3608a7cb42a1e5c91ab5
Robeto Melo <rmello.filho@hotmail.com>
2024-02-01 14:51:46 -0300

    refatorando

--- advinhacao.py
+++ advinhacao.py
@@ -4,16 +4,25 @@ print("===============================")
 
 
 numero_secreto = 42
+total_de_tentativas = 3
 
-chute = int(input("Digite o seu número: "))
+while (total_de_tentativas > 0):
+    print("Tentativa", total_de_tentativas)
+    chute = int(input("Digite o seu número: "))
 
-acertou = chute == numero_secreto
-maior   = chute > numero_secreto
-menor   = chute < numero_secreto
+    acertou = chute == numero_secreto
+    maior   = chute > numero_secreto
+    menor   = chute < numero_secreto
 
-if (acertou):
-    print("Você acertou!", "\nO número secreto é o", chute, end="!")
-elif(maior):
-    print("Você errou!", "\nO seu chute foi", chute, "e ele é maior que o número secreto!")
-elif(menor):
-    print("Você errou!", "\nO seu chute foi", chute, "e ele é menor que o número secreto!")
+    if (acertou):
+        print("Você acertou!", "\nO número secreto é o", chute, end="!")
+    elif(maior):
+        chute        
+        print("Você errou!", "\nO seu chute foi", chute, "e ele é maior que o número secreto!", "\nTente novamente.")
+    elif(menor):
+        chute
+        print("Você errou!", "\nO seu chute foi", chute, "e ele é menor que o número secreto!", "\nTente novamente.")
+
+    total_de_tentativas = total_de_tentativas - 1
+
+    
