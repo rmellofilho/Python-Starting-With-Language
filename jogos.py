a324857bfe0646b94b5f9e1b926168883f168335
Robeto Melo <rmello.filho@hotmail.com>
2024-02-06 19:14:19 -0300

    Módulo para escolher jogo

---
+++ jogos.py
@@ -0,0 +1,10 @@
+print("===============================")
+print("******Escolha o seu jogo!******")
+print("===============================")
+
+jogo = int(input("Qual jogo? "))
+
+if(jogo == 1):
+    print("Jogando forca")
+elif(jogo == 2):
+    print("Jogando advinhação")
